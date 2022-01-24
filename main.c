a19b9a8fef55adb65ac25b4364c6462acc9af42c
June <mhn06005@naver.com>
2022-01-24 16:51:08 +0900

    the first

---
+++ main.c
@@ -0,0 +1,26 @@
+// ULTIMATE  :  UFC - lets do fight in virtual June's brain
+// 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void ConfirmFighters(void);
+double Fighting(int x,int y);
+int main(void) {
+
+	// confirm the warriorses .txt(append / edit / delete)
+	ConfirmFighters();
+
+	// start game
+	// pick opponents
+
+	// fighting()
+	int x, y;
+	Fighting(x, y);
+	// After fighted, renew ranking
+	//AfterFight();
+
+	// # do txt file about fighters(ranking , stats)
+
+	return 0;
+}
