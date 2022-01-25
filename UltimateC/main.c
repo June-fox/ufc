5a5d3d883fdff3b2f2f03f150a550684b9d0f26e
June-fox <73296509+June-fox@users.noreply.github.com>
2022-01-25 15:58:04 +0900

    Add files via upload

--- UltimateC/main.c
+++ UltimateC/main.c
@@ -1,8 +1,5 @@
-// ULTIMATE  :  UFC 선수명단을 꾸리고 가상전투를 펼치자
+// ULTIMATE  :  UFC - lets do fight in virtual June's brain
 // 
-// 1. 단계별 세분화
-// 2. 라이브러리 함수를 꾸밀것(main.c 에는 호출만하겠지)
-// 3. 함수, 변수명은 직관적이고 이해하기 좋은 일정한 규칙을 가진걸로 하자
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -11,19 +8,19 @@ void ConfirmFighters(void);
 double Fighting(int x,int y);
 int main(void) {
 
-	// 선수 명단 확인(추가 / 수정 / 삭제)
+	// confirm the warriorses .txt(append / edit / delete)
 	ConfirmFighters();
 
-	// 게임 시작
-	// 선수 2명 뽑기
+	// start game
+	// pick opponents
 
 	// fighting()
 	int x, y;
 	Fighting(x, y);
-	// 게임 결과로 순위 바뀔때
-	AfterFight
+	// After fighted, renew ranking
+	//AfterFight();
 
-	// 선수 명단 .txt 파일로 저장
+	// # do txt file about fighters(ranking , stats)
 
 	return 0;
 }
