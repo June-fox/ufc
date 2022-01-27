a6c60fa1c8e2d9bbe0d1a8b24224f4a8bed7ac81
June-fox <mhn06005@naver.com>
2022-01-27 16:30:59 +0900

    append func

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 void ConfirmFighters(void);
 double Fighting(int x,int y);
+void ShowMenu(void);
 int main(void) {
 
 	// confirm the warriorses .txt(append / edit / delete)
@@ -23,4 +24,24 @@ int main(void) {
 	// # do txt file about fighters(ranking , stats)
 
 	return 0;
+}
+
+void ConfirmFighters(void) {
+
+	int n = 0;
+	do {
+		// print the menu
+		ShowMenu();
+		// select the menu
+
+		// do menu
+	}while(n == 5)
+}
+
+void ShowMenu(void) {
+	printf("1. Show the fighter chart.");
+	printf("2. Append the fighter.");
+	printf("3. Edit the fighter.");
+	printf("4. Delete the fihter.");
+	printf("5. Let's go on the octagon.");
 }
