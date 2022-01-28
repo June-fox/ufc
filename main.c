d813177c6565f4694910f9d4b9d3ceeceafafcc6
June-fox <mhn06005@naver.com>
2022-01-28 16:19:06 +0900

    make fighter structor

--- main.c
+++ main.c
@@ -3,10 +3,25 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void ConfirmFighters(void);
 double Fighting(int x,int y);
 void ShowMenu(void);
+char* GetString(void);
+
+
+typedef struct fighter {
+	char* name;
+	double age;
+	double strength;
+	double striking;
+	double ground;
+	double clinch;
+	double submission;
+	double stamina;
+}fighterT;
 int main(void) {
 
 	// confirm the warriorses .txt(append / edit / delete)
@@ -44,4 +59,15 @@ void ShowMenu(void) {
 	printf("3. Edit the fighter.");
 	printf("4. Delete the fihter.");
 	printf("5. Let's go on the octagon.");
+}
+
+char* GetString(void) {
+	char s[30] = {0};
+	printf("Which fighter?");
+	scanf("%[^\n]", s);
+
+	if (s[0] == '\n') { return 0; }
+	char* s1 = (char*)malloc(strlen(s) + 1);
+	strcpy(s1, s);
+	return s1;
 }
